e775afa4c5fa3bce03c0830c0d007cdcf04df0b1
atreyuwittman <awitt002@ucr.edu>
2017-05-10 21:31:06 -0700

    Update multi_command.sh

--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -3,4 +3,6 @@ echo dog && echo cat
 echo dog || echo cat
 echo dog || echo cat || exit
 echo dog || echo dog && exit
+mkdir test || mkdir Lite
+ls -a || echo A && echo eagle
 echo dog; echo cat; exit
